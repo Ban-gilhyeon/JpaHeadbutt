30352a6c3986cfc831fc9112d5305b6a33544937
Ban-gilhyeon <bbgiloo98@gmail.com>
2024-09-18 08:12:25 +0900

    feat: Board Controller, BoardEntity 생성

--- src/main/java/org/example/board/controller/BoardController.java
+++ src/main/java/org/example/board/controller/BoardController.java
@@ -1,4 +1,22 @@
 package org.example.board.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class BoardController {
+
+    @GetMapping("board/write")
+    public String write() {
+        return "boardWrite";
+    }
+
+    @PostMapping("board/writepro")
+    public String writepro(String title, String content){
+        System.out.println("제목 : " + title);
+        System.out.println("내용 : " + content);
+
+        return " ";
+    }
 }
--- src/main/java/org/example/board/entity/Board.java
+++ src/main/java/org/example/board/entity/Board.java
@@ -1,4 +1,20 @@
 package org.example.board.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Getter
+@RequiredArgsConstructor
 public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String content;
 }
